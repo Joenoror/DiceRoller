8023a97ae167dba10f018399cd7eb833a19439bd
Joenoror <falanhx@gmail.com>
2022-04-29 13:40:49 +0200

    Toast show

--- app/src/main/java/com/joenoror/diceroller/MainActivity.kt
+++ app/src/main/java/com/joenoror/diceroller/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.joenoror.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Make the button interactable
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+            //You can unify both previous sentences as -->
+            //Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
